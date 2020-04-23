e9376e5adfc40657407ec174fe59b75cc07bea67
Nikita Chibrikov <oldnick1999@mail.ru>
2020-04-23 13:20:45 +0200

    add lab 3 for C# - Triangle testing

---
+++ lab3/UnitTestTriangle/UnitTest1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using lab3;
+
+namespace UnitTestTriangle
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            bool result = Program.TriangleExeption(2, 3, 8);
+            Assert.AreEqual(false, result);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            bool result = Program.TriangleExeption(-2, 3, 4);
+            Assert.AreEqual(false, result);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            bool result = Program.TriangleExeption(10, 100, 1);
+            Assert.AreEqual(false, result);
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            bool result = Program.TriangleExeption(3, 4, 5);
+            Assert.AreEqual(true, result);
+        }
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            bool result = Program.TriangleExeption(2, 2, 1);
+            Assert.AreEqual(true, result);
+        }
+
+        [TestMethod]
+        public void TestMethod6()
+        {
+            bool result = Program.TriangleExeption(1, 1, 1);
+            Assert.AreEqual(true, result);
+        }
+
+        [TestMethod]
+        public void TestMethod7()
+        {
+            bool result = Program.TriangleExeption(-2, -3, -4);
+            Assert.AreEqual(false, result);
+        }
+
+        [TestMethod]
+        public void TestMethod8()
+        {
+            bool result = Program.TriangleExeption(2, -3, 0);
+            Assert.AreEqual(false, result);
+        }
+
+        [TestMethod]
+        public void TestMethod9()
+        {
+            bool result = Program.TriangleExeption(2, 0, 4);
+            Assert.AreEqual(false, result);
+        }
+        [TestMethod]
+        public void TestMethod10()
+        {
+            bool result = Program.TriangleExeption(0, -3, 4);
+            Assert.AreEqual(false, result);
+        }
+    }
+}
---
+++ lab3/lab3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace lab3
+{
+    public class Program
+    {
+        public static bool TriangleExeption (int a, int b, int c)
+        {
+            if (a+b>c && a+c>b && c+b>a && a>=0 && b>=0 && c >= 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Demo of Unit-testing in C#.");
+        }
+    }
+}
